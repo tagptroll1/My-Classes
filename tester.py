8633316dab3dc2a92c151243961369d1ae81896c
Tagptroll1 <thomas@galehuset.org>
2018-06-14 08:17:48 +0200

    fixed logger

--- tester.py
+++ tester.py
@@ -2,18 +2,22 @@ import logging
 
 from matrix import Matrix
 
-logging.basicConfig(filename="tests.log", level=logging.DEBUG,
-                    format="%(asctime)s:%(levelname)s:%(message)s")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="tests.log", encoding="utf-8", mode="w")
+handler.setFormatter(logging.Formatter(
+    "%(asctime)s:%(levelname)s:%(message)s"))
+logger.addHandler(handler)
 
 mat = Matrix(3, 3, values=2)
 mat2 = Matrix(3, 3, values=6)
 
-logging.debug(f"Created Matrix: mat\n{mat}")
-logging.debug(f"Created Matrix: mat2\n{mat2}")
+logger.debug(f"Created Matrix: mat\n{mat}")
+logger.debug(f"Created Matrix: mat2\n{mat2}")
 
-logging.debug("performing mat3 = mat2/mat")
+logger.debug("performing mat3 = mat2/mat")
 mat3 = mat2 / mat
-logging.debug(f"mat3\n{mat3}")
-logging.debug("performing mat2/=mat")
+logger.debug(f"mat3\n{mat3}")
+logger.debug("performing mat2/=mat")
 mat2 /= mat
-logging.debug(f"mat2\n{mat2}")
+logger.debug(f"mat2\n{mat2}")
