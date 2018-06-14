ad15cb2f23d97ef9efcf473681c76398ddc00143
Tagptroll1 <thomas@galehuset.org>
2018-06-14 08:04:53 +0200

    Swapped to logging over print statements

--- tester.py
+++ tester.py
@@ -1,12 +1,19 @@
+import logging
+
 from matrix import Matrix
+
+logging.basicConfig(filename="tests.log", level=logging.DEBUG,
+                    format="%(asctime)s:%(levelname)s:%(message)s")
 
 mat = Matrix(3, 3, values=2)
 mat2 = Matrix(3, 3, values=6)
 
-print(mat)
-print(mat2)
+logging.debug(f"Created Matrix: mat\n{mat}")
+logging.debug(f"Created Matrix: mat2\n{mat2}")
 
+logging.debug("performing mat3 = mat2/mat")
 mat3 = mat2 / mat
-print(mat3)
+logging.debug(f"mat3\n{mat3}")
+logging.debug("performing mat2/=mat")
 mat2 /= mat
-print(mat2)
+logging.debug(f"mat2\n{mat2}")
